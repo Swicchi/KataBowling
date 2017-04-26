f7df3bfdab9bf1248c661899cc251ab6995551de
remferna <remferna@alumnos.ubiobio.cl>
2017-04-26 12:39:08 -0300

    FirstTenSecondOneThirdTwoShouldBySixteen

--- src/main/java/ubb/cl/Bowling.java
+++ src/main/java/ubb/cl/Bowling.java
@@ -2,14 +2,47 @@ package ubb.cl;
 
 public class Bowling {
 	int score;
+	int cantRoll;
+	int frame;
+	boolean strike;
+	int strikePos;
+	int strikeTurn;
+
+	int [] frameScore= new int [10];
 	public Bowling (){
 		score = 0;
+		cantRoll = 0;
+		frame = 0;
+		strike = false;
 	}
 	public void roll(int i) {
-		score+=i;
+		if(i == 10 && cantRoll==0){
+			frameScore[frame]=10;
+			strikePos = frame; 
+			strike = true;
+			frame++;
+			strikeTurn=0;
+		}else{
+			frameScore[frame]+=i;
+			if (strike){
+				frameScore[strikePos]+=i;
+				strikeTurn++;
+				if(strikeTurn==2)
+					strike=false;
+			}
+			cantRoll++;
+			if (cantRoll==2){
+				frame++;
+				cantRoll=0;
+			}
+		}
 	}
 	public int score(){
-		return score;
+		int total =0;
+		for (int i=0;i<10;i++){
+			total+=frameScore[i];
+		}
+		return total;
 	}
 
 }
--- src/test/java/ubb/cl/TestBowling.java
+++ src/test/java/ubb/cl/TestBowling.java
@@ -19,4 +19,29 @@ public void FirstOneSecondThreeScoreShouldByFour(){
 	assertEquals(4,result);
 	
 }
+@Test
+public void FirstTenSecondOneThirdTwoShouldBySixteen(){
+	Bowling bowling = new Bowling();
+	bowling.roll(10);
+	bowling.roll(1);
+	bowling.roll(2);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	bowling.roll(0);
+	int result = bowling.score();
+	assertEquals(16,result);
+}
 }
